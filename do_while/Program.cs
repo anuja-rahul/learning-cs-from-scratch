4495b71c8b9b27bb28c525ca736533ad8f332e92
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-04 19:31:38 +0530

    changes

--- do_while/Program.cs
+++ do_while/Program.cs
@@ -1,9 +1,48 @@
-﻿Random random = new Random();
-int current = 0;
+﻿// This code loop executes at least one time
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+Random random = new Random();
+int current = random.Next(1, 11);
 
 do
 {
-    // This code executes at least one time
     current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
     Console.WriteLine(current);
 } while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
+
+// Exercise
+
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");

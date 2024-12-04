09c1c0fa107b8a1619fe080c150953380c45149a
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-04 22:27:12 +0530

    changes

--- builtin_string_methods/Program.cs
+++ builtin_string_methods/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
